546e21cdad16fb6a02e77921d110039a4fca3166
Michael Daffin <michael@daffin.io>
2020-12-02 10:24:57 +0000

    Switchs to using include_str macro

--- 2020/src/bin/day01.rs
+++ 2020/src/bin/day01.rs
@@ -1,29 +1,40 @@
+use anyhow::Result;
 use itertools::Itertools;
-use std::fs::File;
-use std::io::{self, BufRead};
 
-fn main() {
-    let file = File::open("day01.txt").unwrap();
-    let numbers = io::BufReader::new(file)
+const INPUT: &'static str = include_str!("day01.txt");
+
+fn main() -> Result<()> {
+    let numbers = INPUT
         .lines()
-        .map(|l| l.unwrap().parse::<u32>().unwrap())
-        .collect::<Vec<_>>();
-
-    part1(&numbers);
-    part2(&numbers);
+        .map(|l| l.parse().expect("could not parse input"))
+        .collect::<Vec<u32>>();
+    println!("Part 1: {}", part1(&numbers));
+    println!("Part 2: {}", part2(&numbers));
+    Ok(())
 }
 
-fn part1(numbers: &[u32]) {
+#[adventofcode::part("input/day01.txt")]
+pub fn part1(numbers: &[u32]) -> u32 {
     let (n1, n2) = numbers
         .iter()
         .cartesian_product(numbers.iter())
         .find(|(n1, n2)| *n1 + *n2 == 2020)
         .unwrap();
 
-    println!("{}", n1 * n2);
+    n1 * n2
 }
 
-fn part2(numbers: &[u32]) {
+#[test]
+fn part1_test() {
+    let numbers = INPUT
+        .lines()
+        .map(|l| l.parse().expect("could not parse input"))
+        .collect::<Vec<u32>>();
+    assert_eq!(part1(&numbers), 898299);
+}
+
+#[adventofcode::part("input/day01.txt")]
+fn part2(numbers: &[u32]) -> u32 {
     let ((n1, n2), n3) = numbers
         .iter()
         .cartesian_product(numbers.iter())
@@ -31,5 +42,14 @@ fn part2(numbers: &[u32]) {
         .find(|((n1, n2), n3)| *n1 + *n2 + *n3 == 2020)
         .unwrap();
 
-    println!("{}", n1 * n2 * n3);
+    n1 * n2 * n3
+}
+
+#[test]
+fn part2_test() {
+    let numbers = INPUT
+        .lines()
+        .map(|l| l.parse().expect("could not parse input"))
+        .collect::<Vec<u32>>();
+    assert_eq!(part2(&numbers), 143933922);
 }
